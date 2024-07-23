5288c88b3de0cadb09db804be29fb1f089c10182
Yashvardhan4197 <20BCS4197@cuchd.in>
2024-07-23 12:24:25 +0530

    Added keys in playercontroller through event

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -27,11 +27,13 @@ public class PlayerController
         playerState = PlayerState.InDark;
 
         EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+        EventService.Instance.OnKeysEquipped.AddListener(OnKeyPickedUp);
     }
 
     ~PlayerController()
     {
         EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+        EventService.Instance.OnKeysEquipped.RemoveListener(OnKeyPickedUp);
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
@@ -84,5 +86,10 @@ public class PlayerController
             PlayerState = PlayerState.None;
         else
             PlayerState = PlayerState.InDark;
+    }
+
+    private void OnKeyPickedUp(int keys)
+    {
+        KeysEquipped = keys;
     }
 }
